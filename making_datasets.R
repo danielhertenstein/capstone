1ca4238b97d895bd6cc9b8f7de9d0725491db4d7
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-02-03 14:59:08 -0500

    makes train/test split reproducable

--- making_datasets.R
+++ making_datasets.R
@@ -8,24 +8,33 @@ news <- readLines(file("Coursera-SwiftKey/final/en_US/en_US.news.txt", "rb"), sk
 closeAllConnections()
 
 # Grab a sample of 80%
+set.seed(12)
 samples <- sample(length(news), as.integer(length(news) * 0.8))
 train_news <- news[samples]
 test_news <- news[-samples]
+
+# Save the test set for later
+write(test_news, "test_news.txt")
 
 # Make a test courpus
 train_corpus <- VCorpus(VectorSource(train_news))
 
 # Remove everything
-train_corpus <- tm_map(train_corpus, removeNumbers)
-train_corpus <- tm_map(train_corpus, stripWhitespace)
+# train_corpus <- tm_map(train_corpus, removeNumbers)
+# train_corpus <- tm_map(train_corpus, stripWhitespace)
 train_corpus <- tm_map(train_corpus, content_transformer(tolower))
 # train_corpus <- tm_map(train_corpus, stemDocument)
-train_corpus <- tm_map(train_corpus, removePunctuation)
+# train_corpus <- tm_map(train_corpus, removePunctuation)
 # train_corpus <- tm_map(train_corpus, removeSparseTerms)
 
 # 4-grams
 FourGramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 4, max = 4))
-four_grams <- TermDocumentMatrix(train_corpus, control = list(tokenize = FourGramTokenizer))
+four_grams <- TermDocumentMatrix(train_corpus, control = list(tokenize = FourGramTokenizer,
+                                                              removeNumbers = TRUE,
+                                                              stripWhitespace = TRUE,
+                                                              removePunctuation = TRUE
+                                                              )
+                                 )
 
 # Get the frequencies of each 4-gram
 freqs <- slam::row_sums(four_grams)
