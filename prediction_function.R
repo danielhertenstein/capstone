98be2afc4dc9731ecae92db5f7f9bb4a138cf361
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-02-03 11:06:18 -0500

    add support for using fewer words if all of the input term isn't covered
    if someone inputs 3 words and all three aren't in the dataframe, try
    using the last two words, and if it's still not in there, try just the
    last word

--- prediction_function.R
+++ prediction_function.R
@@ -21,14 +21,24 @@ predict_1 <- function(pieces) {
 
 predict_2 <- function(pieces) {
   possibilities <- combos[(combos$X1 == pieces[1]) & (combos$X2 == pieces[2]), ]
-  combined <- aggregate(possibilities$value, list(x3 = possibilities$X3), sum)
-  combined[order(-combined$x),"x3"][1]
+  if (!nrow(possibilities)) {
+    predict_1(pieces[2])
+  }
+  else {
+    combined <- aggregate(possibilities$value, list(x3 = possibilities$X3), sum)
+    combined[order(-combined$x),"x3"][1]
+  }
 }
 
 predict_3 <- function(pieces) {
   possibilities <- combos[(combos$X1 == pieces[1]) & (combos$X2 == pieces[2]) & (combos$X3 == pieces[3]), ]
-  combined <- aggregate(possibilities$value, list(x4 = possibilities$X4), sum)
-  combined[order(-combined$x),"x4"][1]
+  if (!nrow(possibilities)) {
+    predict_2(pieces[2:3])
+  }
+  else {
+    combined <- aggregate(possibilities$value, list(x4 = possibilities$X4), sum)
+    combined[order(-combined$x),"x4"][1]
+  }
 }
 
 # Can we use corrleation between words in the sentence for unknown n-grams?
