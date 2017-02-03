4908ec842d63c04a1d2d470d79f6e6710a1bdc0d
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-02-03 14:59:30 -0500

    adds ideas for model improvement

--- prediction_function.R
+++ prediction_function.R
@@ -42,3 +42,7 @@ predict_3 <- function(pieces) {
 }
 
 # Can we use corrleation between words in the sentence for unknown n-grams?
+# Replace the first occurrence of every word with <UNK>, then process as normal.
+# Adding <S> and </S> to the beginning and ending of every sentence.
+# Adding 1 to every frequency.
+# What to do when two options have the same frequencies. "I want fish" and "I want tacos" appear an equal amount of times.
