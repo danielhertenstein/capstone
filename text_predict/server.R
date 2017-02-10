0240aee475db8b55c3706ba9d1b8771504c83643
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-02-10 12:35:57 -0500

    iterates on the app code

--- text_predict/server.R
+++ text_predict/server.R
@@ -4,20 +4,100 @@ library(tidyr)
 library(quanteda)
 
 # Load model
+load("en_US.Rda")
+
 # Load profanities
+profanities <- readLines(file("en_profanity.txt", "r"))
+closeAllConnections()
+
+# Set the global variable that lazily contains the top 5 unigrams
+top_five <<- ""
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
-  output$prediction <- renderPlot({
+  output$prediction <- renderText({
     # Get the input string
     in_string <- input$in_string
     
     # Do some sanity checking on the input
     
     # Predict the next word
-    my_predict(in_string)
+    my_predict(combos, in_string)
   })
+  
 })
 
 # Prediction function
+my_predict <- function(model, in_string) {
+  # If the input string is empty, just predict the most common words
+  if (in_string == "") { return(predict_0(model, "")) }
+  
+  # Clean the input string in the same way that was used to build the model to increase the chances of finding a match
+  toks <- tokens(in_string, removeNumbers=TRUE, removePunct=TRUE, removeSymbols=TRUE, removeTwitter=TRUE, removeURL=TRUE)
+  toks <- removeFeatures(toks, profanities)
+  toks <- tokens_tolower(toks)
+  
+  pieces <- unlist(as.list(toks))
+  if (length(pieces) == 1) {
+    predict_1(model, pieces)
+  }
+  else if (length(pieces) == 2) {
+    predict_2(model, pieces)
+  }
+  else if (length(pieces) > 2) {
+    # We only want to use at most the last three words in the input string
+    predict_3(model, tail(pieces, 3))
+  }
+}
+
+predict_0 <- function(model, pieces) { 
+  # This takes a long time, so we only do it once
+  if (top_five == "") {
+    top_five <<- aggregate(combos$value, list(x1 = combos$X1), sum)
+    top_five <<- top_five[order(-top_five$x),"x1"][1:5]
+  }
+  return(top_five)
+}
+
+predict_1 <- function(model, pieces) {
+  possibilities <- model[model$X1 == pieces[1], ]
+  
+  # If no possible matches, return the most common words.
+  if (!nrow(possibilities)) {
+    predict_0(model, pieces)
+  }
+  else {
+    possibilities <- possibilities[!(possibilities$X2 %in% stopwords()), ]
+    combined <- aggregate(possibilities$value, list(x2 = possibilities$X2), sum)
+    combined[order(-combined$x),"x2"][1:5]
+  }
+}
+
+predict_2 <- function(model, pieces) {
+  possibilities <- model[(model$X1 == pieces[1]) & (model$X2 == pieces[2]), ]
+  
+  # If no possible matches, back off to unigrams.
+  if (!nrow(possibilities)) {
+    predict_1(model, tail(pieces, 1))
+  }
+  else {
+    possibilities <- possibilities[!(possibilities$X3 %in% stopwords()), ]
+    combined <- aggregate(possibilities$value, list(x3 = possibilities$X3), sum)
+    combined[order(-combined$x),"x3"][1:5]
+  }
+}
+
+predict_3 <- function(model, pieces) {
+  possibilities <- model[(model$X1 == pieces[1]) & (model$X2 == pieces[2]) & (model$X3 == pieces[3]), ]
+  
+  # If no possibile matches, back off to bigrams.
+  if (!nrow(possibilities)) {
+    predict_2(model, tail(pieces, 2))
+  }
+  else {
+    combined <- aggregate(possibilities$value, list(x4 = possibilities$X4), sum)
+    combined[order(-combined$x),"x4"][1:5]
+  }
+}
--- text_predict/ui.R
+++ text_predict/ui.R
@@ -3,16 +3,22 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Word Prediction App"),
+  titlePanel("The 'How are people talking these days?' App for Time Travelers"),
   
   sidebarLayout(
     sidebarPanel(
       # Put the instructions here
+      h4("This app helps time travelers speak correctly based on the year they time traveled to. Just enter a phrase into the text box, hit Submit, and the app will suggest five different ways to continue your sentence in the way the average person of the year would. Please note that we currently only support time travel to the 2010's.")
     ),
     mainPanel(
       # Input text box
+      textInput("in_string", label="Enter your phrase here", value = "", width = NULL, placeholder = NULL),
+      
+      # Submit button
+      submitButton("Submit"),
       
       # Prediciton text
+      h3(textOutput("prediction"))
     )
   )
 ))
